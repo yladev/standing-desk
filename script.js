28e875c0372363699a790c7ee825fb0a70ef74c7
Yan Linn Aung <yanlinnaung@Yans-MacBook-Air.local>
2024-08-07 22:48:22 +0630

    update nav-bar

---
+++ script.js
@@ -0,0 +1,15 @@
+let closer = document.querySelector('#closer');
+
+closer.onclick = () => {
+    closer.style.display = 'none';
+    navbar.classList.remove('active');
+    cart.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    closer.style.display = "block";
+    navbar.classList.toggle('active');
+}
