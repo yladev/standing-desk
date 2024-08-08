9933929f5e244847d25f8d775ab55446e8db482a
Yan Linn Aung <yanlinnaung@Yans-MacBook-Air.local>
2024-08-08 18:37:48 +0630

    update login form

--- script.js
+++ script.js
@@ -14,9 +14,18 @@ document.querySelector('#menu-btn').onclick = () => {
     navbar.classList.toggle('active');
 }
 
+// shooping cart
 let cart = document.querySelector('.shopping-cart');
 
 document.querySelector('#cart-btn').onclick = () => {
     closer.style.display = "block";
     cart.classList.toggle('active');
+}
+
+// login form
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () => {
+    closer.style.display = "block";
+    loginForm.classList.toggle('active');
 }
