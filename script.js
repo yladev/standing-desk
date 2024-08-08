3a14d3a00c7f6cdeb2323c66ac0dbd4e9f370e72
Yan Linn Aung <yanlinnaung@Yans-MacBook-Air.local>
2024-08-08 14:14:51 +0630

    update shopping cart

--- script.js
+++ script.js
@@ -13,3 +13,10 @@ document.querySelector('#menu-btn').onclick = () => {
     closer.style.display = "block";
     navbar.classList.toggle('active');
 }
+
+let cart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick = () => {
+    closer.style.display = "block";
+    cart.classList.toggle('active');
+}
