9973a10284ffc15d5bc599df1d56180de7098737
Yan Linn Aung <yanlinnaung@Yans-MacBook-Air.local>
2024-08-09 22:44:43 +0630

    update home section

--- js/script.js
+++ js/script.js
@@ -29,3 +29,29 @@ document.querySelector('#login-btn').onclick = () => {
     closer.style.display = "block";
     loginForm.classList.toggle('active');
 }
+
+// home section
+let searchForm = document.querySelector('.header .search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    searchForm.classList.remove('active');
+}
+
+let slides = document.querySelectorAll('.home .slides-container .slide');
+let index = 0;
+
+function next() {
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+function prev() {
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
